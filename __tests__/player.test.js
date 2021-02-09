8ea020765dee525ad40912aff377b9e760e2c563
celaeon <celaeon@gmail.com>
2021-02-08 19:28:07 -0500

    refactored code

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,9 +1,9 @@
 const Potion = require('../lib/Potion');
 jest.mock('../lib/potion');
 
-console.log(new Potion());
+//console.log(new Potion());
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 
 test('create a player object', () => {
     const player = new Player('Dave');
@@ -16,4 +16,24 @@ test('create a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('get players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('get inventory form player or return false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
